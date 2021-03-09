3272809bd7484a6dfa007918ea59078022e17688
KarnaukhovKirill <kirya2361@gmail.com>
2021-03-09 22:41:29 +0300

    Изменил класс Win.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,39 @@
 package ru.job4j.puzzle;
 
 public class Win {
+
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int x = 0; x < board.length; x++) {
+            if (board[row][x] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int y = 0; y < board.length; y++) {
+            if (board[y][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        boolean result = false;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 1) {
+                if (monoHorizontal(board, index) || monoVertical(board, index)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
